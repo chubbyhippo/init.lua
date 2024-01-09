42562bfaad9dae87cf0a725c93cbad88362b297a
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-01-09 21:25:57 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.number = true
 vim.o.ignorecase = true
+vim.o.relativenumber = true
 vim.o.smartcase = true
 -- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
