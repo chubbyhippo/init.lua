80cfa7aeba0fdd7ee605e77e11a4dc4a6480747b
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-01-15 12:37:32 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -14,6 +14,8 @@ vim.o.tabstop = 4
 -- remap
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
