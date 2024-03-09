eb9c31a112c98beebbfa29f0cb8cee5cf2b3522a
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-03-09 10:45:45 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.o.shiftwidth = 4
 vim.o.smartcase = true
 vim.o.smartindent = true
 vim.o.softtabstop = 4
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 vim.o.tabstop = 4
 -- remap
 vim.g.mapleader = ' '
