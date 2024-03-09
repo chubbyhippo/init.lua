f2d8e6efe8a3d2d8fc21784e720160c831ab7799
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-03-09 09:29:00 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -20,19 +20,4 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>ap", vim.cmd.Ex)
 
--- https://github.com/folke/lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-  "folke/which-key.nvim",
-})
+
