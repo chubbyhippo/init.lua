0741d8db05c2615b266fc7f65b8570e6747ed4cc
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2024-07-27 11:54:23 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- color scheme
-vim.cmd.colorscheme 'habamax'
+vim.cmd.colorscheme 'default'
 
 -- set
 vim.o.expandtab = true
@@ -77,7 +77,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "default" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
