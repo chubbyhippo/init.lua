fa7938f6602692d482d1bc6f2e960e7904a1c3ef
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-13 11:46:23 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -121,6 +121,7 @@ vim.keymap.set("n", "<Leader>sf", ":find<CR>", { noremap = true })
 vim.keymap.set("n", "<Leader>st", ":copen<Bar>:grep<Space>", { noremap = true })
 vim.keymap.set("n", "<Leader>sV", ":source $MYVIMRC<CR>", { noremap = true })
 vim.keymap.set("n", "<Leader>w", ":w<CR>", { noremap = true })
+vim.keymap.set("n", "<Leader>q", ":q<CR>", { noremap = true })
 
 -- window sizing
 vim.keymap.set("n", "-", "<c-w>5-")
