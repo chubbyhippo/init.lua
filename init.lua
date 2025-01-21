acf4c97bf0cec67646245c5f40a9333bc5c688b4
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-21 13:46:30 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -152,7 +152,7 @@ vim.keymap.set("v", "(", "c(<Esc>pa)<Esc>")
 vim.keymap.set("v", ")", "c(<Esc>pa)<Esc>")
 vim.keymap.set("v", "[", "c[<Esc>pa]<Esc>")
 vim.keymap.set("v", "]", "c[<Esc>pa]<Esc>")
-
+vim.keymap.set("v", "{", "c{<Esc>pa}<Esc>")
 
 -- status line
 vim.o.statusline = [[%<%f %h%m%r %y%=%{v:register} %-14.(%l,%c%V%) %P]]
