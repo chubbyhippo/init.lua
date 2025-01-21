b39d9673676e162bbea46e298169d643f1d4c248
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-21 12:38:48 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -147,6 +147,7 @@ vim.keymap.set("i", "<c-l>", "<c-w><c-l>")
 
 -- surround with
 vim.keymap.set("v", "'", "c'<Esc>pa'<Esc>")
+vim.keymap.set("v", "\"", "c\"<Esc>pa\"<Esc>")
 
 -- status line
 vim.o.statusline = [[%<%f %h%m%r %y%=%{v:register} %-14.(%l,%c%V%) %P]]
